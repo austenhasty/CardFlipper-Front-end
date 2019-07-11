386677ed9492d8d6dd9a0712db1b65a04e4d7e66
austenhasty <hastyae@gmail.com>
2019-07-11 16:26:29 -0700

    added shuffle logic and a couple other things

---
+++ card-flipper/src/components/ShowClicks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const ShowClicks = (props) =>{
+
+  return (
+  // <Card id="clicks">
+    <div className="click-div">Clicks Remaining: {props.clicks}</div>
+  // </Card>
+  )
+}
+
+export default ShowClicks
