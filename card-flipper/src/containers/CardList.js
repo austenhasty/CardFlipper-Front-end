5a3a238925164e8c182aead2641e2dff6012c23c
austenhasty <hastyae@gmail.com>
2019-07-12 11:15:30 -0700

    added modal...still need to style

--- card-flipper/src/containers/CardList.js
+++ card-flipper/src/containers/CardList.js
@@ -3,6 +3,7 @@ import { Card } from 'semantic-ui-react'
 import NewGame from '../components/NewGame'
 import ShowCard from '../components/ShowCard'
 import ShowClicks from '../components/ShowClicks'
+import Modal from './Modal'
 import a from '../components/images/ak.jpg'
 import b from '../components/images/aTrain.jpeg'
 import c from '../components/images/cTown.jpeg'
@@ -14,6 +15,7 @@ import h from '../components/images/monkey.jpeg'
 import i from '../components/images/theModel.jpeg'
 import j from '../components/images/vForVictory.png'
 import flatiron from '../components/images/flatiron.png'
+import ReactDOM from "react-dom";
 
 const cardImages = [a, b, c, d, e, f, g, h, i, j]
 
@@ -22,7 +24,8 @@ export default class CardList extends Component {
     super()
     this.state = {
       clicks: 10,
-      cardsArray:[]
+      cardsArray:[],
+      showModal: false
     }
   }
 
@@ -58,17 +61,28 @@ export default class CardList extends Component {
     // return deck
   }
 
-  render(){
+  toggleModal= () => {
+    this.setState(prevState => {
+      return {showModal: !prevState.showModal}
+    })
+  }
 
+  render(){
     return (
-      <div onClick={this.handleClicks}>
-        <ShowClicks clicks={this.state.clicks}/>
-        <Card.Group itemsPerRow={5}>
-          {this.state.cardsArray.map((src, index)=> {
-            return <ShowCard key={index} id={index} frontPic={src} backPic={flatiron}/>
-          })}
-        </Card.Group>
-      </div>
+      <React.Fragment>
+        <input type="submit" onClick={this.toggleModal} value="Win" />
+        <input type="submit" onClick={this.toggleModal} value="Loss"/>
+        <Modal showModal={this.state.showModal} toggleModal={this.toggleModal}/>
+          <div onClick={this.handleClicks}>
+            {this.state.clicks === 0 ? window.alert("I'm sorry, but you're a LOSER"):null}
+            <ShowClicks clicks={this.state.clicks}/>
+            <Card.Group itemsPerRow={5}>
+              {this.state.cardsArray.map((src, index)=> {
+                return <ShowCard key={index} id={index} frontPic={src} backPic={flatiron}/>
+              })}
+            </Card.Group>
+          </div>
+        </React.Fragment>
     )
   }
 }
